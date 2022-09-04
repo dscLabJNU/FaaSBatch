a70085940c9a5b36c07693b7b8916c51d57c526c
GitHubDiom <867320164@qq.com>
2022-09-04 08:52:36 +0000

    minor: update azure trece generator 1. Describe workflow as `app` and functions as the corresponding `func`.

--- benchmark/generator/azure-bench/generate_trace.py
+++ benchmark/generator/azure-bench/generate_trace.py
@@ -1,80 +1,78 @@
 import os
 import yaml
-import pandas as pd 
+import pandas as pd
 import sys
 import json
 sys.path.append('../../../config')
 import config
 
-def parse_flat_workflow(format_func_name, flat_workflow):
-    function_info = {'name': format_func_name, "source":format_func_name,"runtime":1,"scale":3,"mem_usage":0.25}
-    flat_workflow['functions'].append(function_info)
+
+def parse_flat_workflow(functions, flat_workflow):
+    for func_name in functions:
+        function_info = {'name': func_name, "source": func_name,
+                         "runtime": 1, "scale": 3, "mem_usage": 0.25}
+        flat_workflow['functions'].append(function_info)
     return flat_workflow
 
-def parse_function_info(format_func_name, function_info, workflow_name=None):
-    function_info['workflow'] = workflow_name or format_func_name
-    function_info['functions'].append({
-        "image": "azure-bench",
-        "name": format_func_name,
-        'qos_requirement': 0.95,
-        'qos_time': 100
-    })
+
+def parse_function_info(workflow_name, functions, function_info):
+    function_info['workflow'] = workflow_name
+    for func_name in functions:
+        function_info['functions'].append({
+            "image": "azure-bench",
+            "name": func_name,
+            'qos_requirement': 0.95,
+            'qos_time': 100
+        })
     return function_info
 
-def write_yamls(flat_workflow, function_info, format_func_name):
-    workflow_dir = f"./workflows/{format_func_name}"
+
+def write_yamls(flat_workflow, function_info, workflow_name):
+    workflow_dir = f"./workflows/{workflow_name}"
     if not os.path.exists(workflow_dir):
         os.makedirs(workflow_dir)
 
-        
     flat_workflow_yaml = open(f"{workflow_dir}/flat_workflow.yaml", 'w', encoding='utf-8')
     yaml.dump(flat_workflow, flat_workflow_yaml, sort_keys=False)
 
     function_info_yaml = open(f"{workflow_dir}/function_info.yaml", 'w', encoding='utf-8')
     yaml.dump(function_info, function_info_yaml, sort_keys=False)
-    
 
-def generate_workflows(parse_flat_workflow, parse_function_info, write_yamls, df):
-    # 将func转化可读的为function_name，编号从00000001开始
-    func_map = {func: f"azure_bench_{num+1:08}" for num, func in enumerate(df['func'].unique())}
-    map_json = json.dumps(func_map)
-    json_file = open("./func_mapper.json", 'w')
-    json_file.write(map_json)
+
+def generate_workflows(df):
 
     with open("./func_mapper.json") as load_f:
         func_map_dict = json.load(load_f)
 
-    for func in df['func'].unique():
-    # Parse each specific workflow
+    with open("./app_mapper.json") as load_f:
+        app_map_dict = json.load(load_f)
+
+    for app, data in df.groupby('app'):
         flat_workflow = {'functions': []}
         function_info = {'workflow': "", 'max_containers': 5, 'functions': []}
-        format_func_name = func_map_dict[func]
-        print('\r' + f'generating workflow {format_func_name}...', end='', flush=True)
+        workflow_name = app_map_dict[app]
+        print(f'generating workflow {workflow_name}')
 
-        flat_workflow = parse_flat_workflow(format_func_name, flat_workflow)
-        function_info = parse_function_info(format_func_name, function_info)
-        write_yamls(flat_workflow, function_info, format_func_name)
+        functions = list(map(lambda x: func_map_dict[x], data['func'].unique()))
+        print(functions)
+        flat_workflow = parse_flat_workflow(functions, flat_workflow)
+        function_info = parse_function_info(workflow_name, functions, function_info)
 
-    print()
-    return func_map_dict
+        write_yamls(flat_workflow, function_info, workflow_name)
 
-def genreate_all_bench():
+def process_and_dump(df):
+    app_map = {app: f"azure_bench_app_{num+1:08}" for num,
+               app in enumerate(df['app'].unique())}
+    func_map = {func: f"azure_bench_func_{num+1:08}" for num,
+                func in enumerate(df['func'].unique())}
+    with open("./func_mapper.json", 'w') as dump_f:
+        dump_f.write(json.dumps(func_map))
 
-    all_flat_workflow = {'functions': []}
-    all_function_info = {'workflow': "", 'max_containers': 5, 'functions': []}
-
-    for func in df['func'].unique():
-        # Parse all workflows, only for registry the function to the couch db
-        format_func_name = func_map_dict[func]
-        print('\r' + f'generating workflow azrure_bench_all ...', end='', flush=True)
-
-        all_flat_workflow = parse_flat_workflow(format_func_name, all_flat_workflow)
-        all_function_info = parse_function_info(format_func_name, all_function_info, "azure_bench_all")
-    write_yamls(all_flat_workflow, all_function_info, 'azure_bench_all')
-    print()
+    with open("./app_mapper.json", 'w') as dump_f:
+        dump_f.write(json.dumps(app_map))
 
 if __name__ == "__main__":
     data_dir = config.AZURE_DATA_DIR
     df = pd.read_csv(f"{data_dir}/AzureFunctionsInvocationTraceForTwoWeeksJan2021.txt")
-    func_map_dict = generate_workflows(parse_flat_workflow, parse_function_info, write_yamls, df)
-    genreate_all_bench()
+    process_and_dump(df)
+    generate_workflows(df)
--- config/config.py
+++ config/config.py
@@ -1,4 +1,5 @@
 import os
+import customize_azure
 COUCHDB_URL = 'http://openwhisk:openwhisk@10.0.0.103:5984/'
 REDIS_HOST = '127.0.0.1' # it serves to connect with the local redis, so it should be 127.0.0.1
 REDIS_PORT = 6379 # it follows the same configuration as created redis by docker (e.g., -p 6379:6379)
@@ -13,7 +14,8 @@ WORKFLOW_YAML_ADDR = {'fileprocessing': '/home/vagrant/batching-request/benchmar
                   'epigenomics': '/home/vagrant/batching-request/benchmark/generator/epigenomics/flat_workflow.yaml',
                   'genome': '/home/vagrant/batching-request/benchmark/generator/genome/flat_workflow.yaml',
                   'soykb': '/home/vagrant/batching-request/benchmark/generator/soykb/flat_workflow.yaml',
-                  'azure_bench_all': '/home/vagrant/batching-request/benchmark/generator/azure-bench/workflows/azure_bench_all/flat_workflow.yaml'}
+                  }
+WORKFLOW_YAML_ADDR.update(customize_azure.AZURE_WORKFLOW_YAML_ADDR)
 NETWORK_BANDWIDTH = 25 * 1024 * 1024 / 4 # 25MB/s / 4
 NET_MEM_BANDWIDTH_RATIO = 15 # mem_time = net_time / 15
 CONTAINER_MEM = 256 * 1024 * 1024 # 256MB
@@ -26,6 +28,7 @@ FUNCTION_INFO_ADDRS = {'genome': '../../benchmark/generator/genome', 'epigenomic
                                                 'soykb': '../../benchmark/generator/soykb', 'cycles': '../../benchmark/generator/cycles',
                                                 'fileprocessing': '../../benchmark/fileprocessing', 'wordcount': '../../benchmark/wordcount',
                                                 'illgal_recognizer': '../../benchmark/illgal_recognizer', 'video': '../../benchmark/video'}
+FUNCTION_INFO_ADDRS.update(customize_azure.AZURE_FUNCTION_INFO_ADDR)
 DATA_MODE = 'raw' # raw, optimized
 CONTROL_MODE = 'WorkerSP' # WorkerSP, MasterSP
 CLEAR_DB_AND_MEM = True
