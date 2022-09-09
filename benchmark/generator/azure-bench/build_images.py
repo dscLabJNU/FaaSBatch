7b7d16178ae68b263093da6f01f0eb0851e57c4a
GitHubDiom <867320164@qq.com>
2022-09-09 03:00:12 +0000

    minor: add script for building Azure images

--- benchmark/generator/azure-bench/build_images.py
+++ benchmark/generator/azure-bench/build_images.py
@@ -10,10 +10,14 @@ def parse_function_info(workflow_path):
 
 
 workflow_paths = glob.glob("./workflows/*")
+rm_containers = 'docker rm -f $(docker ps -aq --filter label=workflow)'
+rm_images = 'docker rmi -f $(docker images -f "reference=azure_bench_func*" --format "{{.ID}}")'
+rm_dangling_images = 'docker rmi -f $(docker images -q -a -f dangling=true)'
+os.system(rm_dangling_images)
+os.system(rm_containers)
+os.system(rm_images)
 for workflow_path in workflow_paths:
     function_info = parse_function_info(workflow_path)
-    rm_images = 'docker rmi -f $(docker images -f "reference=azure_bench_func*" --format "{{.ID}}")'
-    os.system(rm_images)
     # build images
     functions = list(map(lambda x: x, function_info['functions']))
     for function in functions:
--- scripts/worker_restart.bash
+++ scripts/worker_restart.bash
@@ -4,3 +4,7 @@ sudo systemctl daemon-reload
 sudo systemctl restart docker
 
 docker run -itd -p 6379:6379 --name redis redis
+cd ../benchmark/generator/azure-bench/
+python3 generate_trace.py 
+python3 build_images.py
+cd -
