fec70e4caf3c3a5b083f1b2ff9cacdc7a3d74dd9
GitHubDiom <867320164@qq.com>
2022-09-13 07:48:54 +0000

    minor: implement paralized inside container

--- src/container/proxy.py
+++ src/container/proxy.py
@@ -1,4 +1,5 @@
 import os
+import threading
 import time
 import couchdb
 from flask import Flask, request
@@ -6,6 +7,7 @@ from gevent.pywsgi import WSGIServer
 from Store import Store
 import container_config
 import redis
+from main import main as __main__
 
 default_file = 'main.py'
 work_dir = '/proxy'
@@ -108,6 +110,42 @@ def run():
     proxy.status = 'ok'
     return res
 
+@proxy.route('/batch_run', methods=['POST'])
+def batch_run():
+    proxy.status = 'run'
+    inps = request.get_json(force=True, silent=True)
+    responses = {}
+    threads = []
+    for inp in inps:
+        t = threading.Thread(target=run_single, args=(inp, responses,))
+        threads.append(t)
+        t.start()
+        
+    for t in threads:
+        t.join()
+    proxy.status = 'ok'
+    return responses
+
+def run_single(inp, responses):
+    request_id = inp['request_id']
+    runtime = inp['runtime']
+    input = inp['input']
+    output = inp['output']
+    to = inp['to']
+    keys = inp['keys']
+
+    # record the execution time
+    start = time.time()
+    runner.run(request_id, runtime, input, output, to, keys)
+    end = time.time()
+
+    responses[request_id] = {
+        "start_time": start,
+        "end_time": end,
+        "duration": end - start,
+        "inp": inp
+    }
+    
 
 if __name__ == '__main__':
     server = WSGIServer(('0.0.0.0', 5000), proxy)
