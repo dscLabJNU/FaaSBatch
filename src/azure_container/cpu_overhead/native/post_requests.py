6a7e6d457cc8ee2c04d4b1ae91059c13e3ce91fe
GitHubDiom <867320164@qq.com>
2022-10-24 14:02:26 +0000

    minor: update cpu_native proxy

--- src/azure_container/cpu_overhead/native/post_requests.py
+++ src/azure_container/cpu_overhead/native/post_requests.py
@@ -4,11 +4,11 @@ import requests
 base_url = 'http://127.0.0.1:{}/{}'
 
 concurrency = 5
-batch_reqs = [{"duration": 0.01, "function_id": str(id), "concurrency": concurrency} for id in range(1, concurrency+1)]
+batch_reqs = [{"duration": 0.01, "function_id": str(id), "concurrency": concurrency, "input_n": 34} for id in range(1, concurrency+1)]
 single_req = {"duration": 0.01, "function_id": "1", "concurrency": 1}
 
-r = requests.post(base_url.format(5000, 'run'), json=single_req)
-# r = requests.post(base_url.format(5000, 'batch_run'), json=batch_reqs)
+# r = requests.post(base_url.format(5000, 'run'), json=single_req)
+r = requests.post(base_url.format(5000, 'batch_run'), json=batch_reqs)
 
 print(r.json())
 
--- src/azure_container/cpu_overhead/native/proxy.go
+++ src/azure_container/cpu_overhead/native/proxy.go
@@ -1,12 +1,50 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
+
+type FibResult struct {
+	StartTime float64 `json:"start_time"`
+	Duration  float64 `json:"duration"`
+	EndTime   float64 `json:"end_time"`
+	Result    int     `json:"result"`
+	WaitTime  int64   `json:"wait_time"`
+}
+
+func invokeFunction(req map[string]interface{}, responses map[string]interface{}) string {
+	functionId, _ := req["function_id"].(string)
+
+	var schedParams = []string{"python3", "fib.py"}
+	if _, ok := req["input_n"]; ok {
+		inputN := strconv.Itoa(int(req["input_n"].(float64)))
+		schedParams = append(schedParams, inputN)
+		// log.Printf("Runing python fip.py %s\n", inputN)
+	}
+	log.Println("cmdParams:", schedParams)
+
+	cmd := exec.Command("sudo", schedParams...)
+	out, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var fibResult FibResult
+	err = json.Unmarshal(out, &fibResult)
+	if err != nil {
+		log.Println("Failed because: ", err)
+	}
+	responses[functionId] = fibResult
+	return functionId
+}
 
 // This function recieves one request each time
 func run(c *gin.Context) {
@@ -15,20 +53,31 @@ func run(c *gin.Context) {
 	log.Println("req: ", req)
 	responses := make(map[string]interface{}, 1)
 
-	value, _ := req["function_id"].(string)
+	function_id, _ := req["function_id"].(string)
 
-	responses[value] = run_main()
+	responses[function_id] = run_main()
 	c.JSON(http.StatusOK, responses)
 }
 
 // This function recieves serverl requests each time, but executes in blocking
 func batch_run(c *gin.Context) {
-	reqs := make([]map[string]string, 0)
+	reqs := make([]map[string]interface{}, 0)
 	c.BindJSON(&reqs)
 	log.Print("len of reqs: ", len(reqs))
 	responses := make(map[string]interface{})
+	startTime := time.Now()
 	for _, req := range reqs {
-		responses[req["function_id"]] = run_main()
+		functionId := invokeFunction(req, responses)
+		waitTime := time.Since(startTime)
+
+		if entry, ok := responses[functionId].(FibResult); ok {
+			// Modify the copy
+			entry.WaitTime = waitTime.Milliseconds()
+			// Reassign map entry
+			responses[functionId] = entry
+		}
+		log.Println("Funciton", functionId, "has been waited for", waitTime)
+		log.Println("response:", responses[functionId])
 	}
 
 	c.JSON(http.StatusOK, responses)
