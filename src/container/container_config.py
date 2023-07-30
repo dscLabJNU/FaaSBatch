0f1fb9d2618b92fe22c198b86beccd47c89ae00e
GitHubDiom <867320164@qq.com>
2023-07-30 08:02:39 +0000

    Get container config automatically

--- src/container/container_config.py
+++ src/container/container_config.py
@@ -1,9 +1,17 @@
-# ======= NEED TO CONFIG =======
-MASTER_IP="192.168.1.18"
-DOCKER_0_IP = "172.17.0.1" # it serves to communicate with the host-side components from the docker container, and it should be 172.17.0.1
-# ======= NEED TO CONFIG =======
+import sys
+import socket
+import fcntl
+import struct
+sys.path.append('../../config')
+import config
 
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915,  # SIOCGIFADDR
+                            struct.pack('256s', ifname[:15].encode('utf-8')))[20:24])
 
+MASTER_IP=config.MASTER_IP
+DOCKER_0_IP = get_ip_address('docker0') # it serves to communicate with the host-side components from the docker container, and it should be 172.17.0.1
 COUCHDB_URL = f'http://openwhisk:openwhisk@{MASTER_IP}:5984/'
 REDIS_HOST = DOCKER_0_IP
 REDIS_PORT = 6379
